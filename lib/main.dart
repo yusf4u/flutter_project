d10bb9d44af19ba912309a5300b7803718c36f6a
Mohamed Zaghloul <zaghloulmohamed9@gmail.com>
2025-05-26 20:38:13 +0300

    jobs: adjust dir structure and refactor import paths

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'views/auth/verified_account_client.dart';
 import 'features/developer/presentation/screens/developer_info_page.dart';
 import 'views/client/client_info_page.dart';
 import 'views/bank/bank_account_page.dart';
-import 'features/profile/presentation/screens/profile_setting_page.dart';
+import 'views/jobs/profile_setting_page.dart';
 import 'views/home/home_page.dart';
 import 'features/jobs/presentation/screens/website_page.dart';
 import 'features/jobs/presentation/screens/category_details.dart';
